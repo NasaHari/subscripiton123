06902fe37620294d0abcdf9a5b5160cbd3c748fe
NasaHari <56309180+NasaHari@users.noreply.github.com>
2023-07-30 18:48:22 +0530

    changed index

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min'
-import Popper from 'popper.js';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
